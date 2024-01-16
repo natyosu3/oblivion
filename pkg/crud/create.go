4472659026d17f98e5f505b74a2378fbf5cb34ce
natyosu3 <nachi334825@gmail.com>
2024-01-16 12:48:11 +0900

    Refactor user registration and database insertion

--- pkg/crud/create.go
+++ pkg/crud/create.go
@@ -1,18 +1,20 @@
 package crud
 
 import (
-	"database/sql"
 	_ "github.com/lib/pq"
 	"fmt"
 )
 
 
-func InsertUser(db *sql.DB, name string, email string, password string) {
+func InsertUser(name string, email string, password string) error {
+	db := Connect()
+	defer db.Close()
+
 	sqlStatement := `INSERT INTO "User" (username, email, password) VALUES ($1, $2, $3)`
 	_, err := db.Exec(sqlStatement, name, email, password)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	fmt.Println("New user added")
-
+	return nil
 }
--- pkg/engine/auth/auth_handller.go
+++ pkg/engine/auth/auth_handller.go
@@ -1,6 +1,9 @@
 package auth
 
 import (
+	"fmt"
+	"oblivion/pkg/utils/crypto"
+	"oblivion/pkg/crud"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,6 +22,24 @@ func RegisterGet() gin.HandlerFunc {
 
 func RegisterPost() gin.HandlerFunc {
 	return func (c *gin.Context)  {
-		c.HTML(200, "register.html", gin.H{})
+		username := c.PostForm("username")
+		email := c.PostForm("email")
+		password := c.PostForm("password")
+
+		// パスワードをハッシュ化
+		hash, err := crypto.PasswordEncrypt(password)
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(500, gin.H{"message": "Internal Server Error"})
+		}
+
+		// ユーザー登録
+		err = crud.InsertUser(username, email, hash)
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(500, gin.H{"message": "Internal Server Error"})
+		}
+
+		c.JSON(200, gin.H{"message": "OK"})
 	}
 }

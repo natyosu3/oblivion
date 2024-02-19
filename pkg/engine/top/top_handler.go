ce7bf7453de5ccf251e076062ea00d9c76c35761
natyosu3 <nachi334825@gmail.com>
2024-02-20 02:18:57 +0900

    セッションを追加し、ユーザーが認証されていない場合にログインページを表示する

--- pkg/engine/top/top_handler.go
+++ pkg/engine/top/top_handler.go
@@ -2,11 +2,18 @@ package top
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/sessions"
 )
 
 
 func Index() gin.HandlerFunc {
 	return func (c *gin.Context)  {
+		session := sessions.Default(c)
+		if user := session.Get("user"); user == nil {
+			c.HTML(200, "index.html", gin.H{
+				"IsAuthenticated": false,
+			})
+		}
 		c.HTML(200, "index.html", gin.H{
 			"IsAuthenticated": true,
 		})

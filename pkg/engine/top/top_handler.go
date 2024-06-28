d3f4ae73bd44a368b8d1651d506209ae833ba5db
natyosu3 <nachi334825@gmail.com>
2024-06-28 12:10:05 +0900

    sessionが空の際のエラーハンドリングを修正

--- pkg/engine/top/top_handler.go
+++ pkg/engine/top/top_handler.go
@@ -9,7 +9,7 @@ import (
 
 func Index() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		data := session.Default(c, "session", &model.Session_model{}).Get(c).(*model.Session_model)
+		data := session.Default(c, "session", &model.Session_model{}).Get(c)
 		if data == nil {
 			c.HTML(200, "index.html", gin.H{
 				"IsAuthenticated": false,
